54f70e4ffa6238b170c43bd66996a682c55e858d
Norman Kean <6553711+nkean97@users.noreply.github.com>
2022-10-05 15:55:36 -0500

    Add files via upload

---
+++ EC2/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum install -y httpd php git
+service httpd start
+cd
+git clone https://github.com/nkean97/Class_Demos.git
+cp ./Class_Demos/EC2/* /var/www/html
+mv /var/www/html/htaccess /var/www/html/.htaccess
