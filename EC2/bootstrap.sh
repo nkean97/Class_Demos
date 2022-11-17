994a6d2cb825968ff4197f676651f09a87451b97
Norman Kean <6553711+nkean97@users.noreply.github.com>
2022-11-17 07:58:17 -0600

    Added health page

--- EC2/bootstrap.sh
+++ EC2/bootstrap.sh
@@ -5,3 +5,5 @@ cd
 git clone https://github.com/nkean97/Class_Demos.git
 cp ./Class_Demos/EC2/* /var/www/html
 mv /var/www/html/htaccess /var/www/html/.htaccess
+echo "Healthy" > /var/www/html/health.html
+yum update -y
