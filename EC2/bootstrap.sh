93207cc0e67af1e33073e9a3ac34883ebaa2a978
Norman Kean <6553711+nkean97@users.noreply.github.com>
2023-03-14 15:20:26 -0500

    Delete bootstrap.sh

--- EC2/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-yum install -y httpd php git
-service httpd start
-cd
-git clone https://github.com/nkean97/Class_Demos.git
-cp ./Class_Demos/EC2/* /var/www/html
-mv /var/www/html/htaccess /var/www/html/.htaccess
-echo "Healthy" > /var/www/html/health.html
-yum update -y
