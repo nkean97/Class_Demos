0386a07d276c28c3498f5151d0521d444163ffdd
Norm Kean <normakea@amazon.com>
2023-08-03 05:30:52 -0500

    Updated user data demo

--- EC2/user_data.sh
+++ EC2/user_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 dnf install -y httpd php stress git
 dnf update -y --security
+systemctl enable httpd.service
+systemctl start httpd.service
 cd
 git clone https://github.com/nkean97/Class_Demos.git
 cp ./Class_Demos/EC2/* /var/www/html
 mv /var/www/html/htaccess /var/www/html/.htaccess
-systemctl enable httpd.service
-systemctl start httpd.service
 echo "Healthy" > /var/www/html/health.html
 dnf update -y
